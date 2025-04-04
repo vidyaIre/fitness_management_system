a0feaf3924800515fd37c5c94f93829270b92af6
vidya <vidya.palliyalil@gmail.com>
2025-04-04 20:06:46 +0530

    implement nutrition management features; add CRUD operations for nutrition data and corresponding routes

--- Backend/controllers/nutritionController.js
+++ Backend/controllers/nutritionController.js
@@ -1,6 +1,8 @@
+const e = require('express');
 const nutrition = require('../models/nutritionModel');
 const User = require('../models/userModel');
 
+// Create a new nutrition 
 exports.createNutrition = async (req, res) => {
     const { assignedTo, meals, food_items, total_calories, createdBy } = req.body;
     console.log("assignedTo:", req.body.assignedTo);
@@ -28,7 +30,7 @@ exports.createNutrition = async (req, res) => {
             assignedTo: userId,
             meals: meals.map(meal => ({
                 meal_time: meal.meal_time,
-                
+
                 food_items: meal.food_items.map(food_item => ({
                     food_name: food_item.food_name,
                     quantity: food_item.quantity,
@@ -67,3 +69,130 @@ exports.createNutrition = async (req, res) => {
 
     }
 };
+// Get all nutrition
+exports.getAllNutrition = async (req, res) => {
+    try {
+        const nutritionList = await nutrition.find().populate('assignedTo', 'name email').populate('createdBy', 'name email');
+        res.status(200).json({
+            success: true,
+            statusCode: 200,
+            count: nutritionList.length,
+            message: "Nutrition retrieved successfully",
+            nutrition: nutritionList
+        });
+    } catch (error) {
+        console.error("Error retrieving nutrition:", error);
+        res.status(500).json({
+            success: false,
+            statusCode: 500,
+            message: "Server error"
+        });
+    }
+};
+// Get nutrition by user ID
+exports.getNutritionByUserId = async (req, res) => {
+    const { id } = req.body;
+    console.log("nutrition id:", id);
+
+
+    try {
+        if (!id) {
+            return res.status(400).json({
+                success: false,
+                statusCode: 400,
+                message: "User ID is required"
+            });
+        }
+        const nutritionList = await nutrition.find({ _id: id }).populate('_id', 'name email').populate('createdBy', 'name email');
+
+        if (!nutritionList || nutritionList.length === 0) {
+            return res.status(404).json({
+                success: false,
+                statusCode: 404,
+                message: "No nutrition found for this user"
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            statusCode: 200,
+            count: nutritionList.length,
+            message: "Nutrition retrieved successfully",
+            nutrition: nutritionList
+        });
+    } catch (error) {
+        console.error("Error retrieving nutrition:", error);
+        res.status(500).json({
+            success: false,
+            statusCode: 500,
+            message: "Server error"
+        });
+    }
+};
+// Update nutrition by ID
+exports.updateNutritionById = async (req, res) => {
+    const { id } = req.body;
+    console.log("nutrition id:", id);
+    const { assignedTo, meals, food_items, total_calories, createdBy } = req.body;
+
+    try {
+        const updatedNutrition = await nutrition.findByIdAndUpdate(id, {
+            assignedTo,
+            meals,
+            food_items,
+            total_calories,
+            createdBy
+        }, { new: true });
+
+        if (!updatedNutrition) {
+            return res.status(404).json({
+                success: false,
+                statusCode: 404,
+                message: "Nutrition not found"
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            statusCode: 200,
+            message: "Nutrition updated successfully",
+            nutrition: updatedNutrition
+        });
+    } catch (error) {
+        console.error("Error updating nutrition:", error);
+        res.status(500).json({
+            success: false,
+            statusCode: 500,
+            message: "Server error"
+        });
+    }
+};
+// Delete nutrition by ID
+exports.deleteNutritionById = async (req, res) => {
+    const { id } = req.body;
+    console.log("nutrition id:", id);
+    try {
+        const deletedNutrition = await nutrition.findByIdAndDelete(id);
+
+        if (!deletedNutrition) {
+            return res.status(404).json({
+                success: false,
+                statusCode: 404,
+                message: "Nutrition not found"
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            statusCode: 200,
+            message: "Nutrition deleted successfully"
+        });
+    } catch (error) {
+        console.error("Error deleting nutrition:", error);
+        res.status(500).json({
+            success: false,
+            statusCode: 500,
+            message: "Server error"
+        });
+    }
+}
--- Backend/routes/nutritionRoute.js
+++ Backend/routes/nutritionRoute.js
@@ -1,7 +1,12 @@
 const express = require('express');
-const { createNutrition } = require('../controllers/nutritionController');
+const { createNutrition, getAllNutrition , getNutritionByUserId, updateNutritionById} = require('../controllers/nutritionController');
 const router = express.Router();
 
 router.post('/createNutrition', createNutrition);
+router.get('/getAllNutrition', getAllNutrition);
+router.get('/getNutritionByUserId', getNutritionByUserId);
+router.put('/updateNutritionById', updateNutritionById);
+
+
 
 module.exports = router;
