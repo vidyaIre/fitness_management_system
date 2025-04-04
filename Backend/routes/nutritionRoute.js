8de5d2b8619518b718d6c6ce08ad9d8983523d21
vidya <vidya.palliyalil@gmail.com>
2025-04-04 23:14:10 +0530

    implement nutrition and workout management features; add delete functionality for nutrition and workout routes

--- Backend/routes/nutritionRoute.js
+++ Backend/routes/nutritionRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createNutrition, getAllNutrition , getNutritionByUserId, updateNutritionById} = require('../controllers/nutritionController');
+const { createNutrition, getAllNutrition , getNutritionByUserId, updateNutritionById, deleteNutritionById} = require('../controllers/nutritionController');
 const authMiddleware = require('../middleware/authMiddleware');
 const router = express.Router();
 
@@ -7,7 +7,7 @@ router.post('/createNutrition',authMiddleware, createNutrition);
 router.get('/getAllNutrition',authMiddleware, getAllNutrition);
 router.get('/getNutritionByUserId',authMiddleware, getNutritionByUserId);
 router.put('/updateNutritionById',authMiddleware, updateNutritionById);
-
+router.delete('/deleteNutritionById',authMiddleware, deleteNutritionById);
 
 
 module.exports = router;
--- Backend/routes/workoutRoute.js
+++ Backend/routes/workoutRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createWorkout, getAllWorkouts, getWorkoutById, updateWorkout } = require('../controllers/workoutController');
+const { createWorkout, getAllWorkouts, getWorkoutById, updateWorkout, deleteWorkout } = require('../controllers/workoutController');
 const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
 
@@ -7,5 +7,6 @@ router.post('/createWorkout',authMiddleware, createWorkout);
 router.get('/getAllWorkouts',authMiddleware, getAllWorkouts);
 router.get('/getWorkoutById', authMiddleware,getWorkoutById);
 router.put('/updateWorkout',authMiddleware, updateWorkout);
+router.delete('/deleteWorkout',authMiddleware, deleteWorkout);
 
 module.exports = router;
